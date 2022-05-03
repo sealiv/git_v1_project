75320db0c9ee08eae164ed4f1b7f063a38a9b09a
Oleksii Sergiienko <sai.79.ua@gmail.com>
2022-05-03 15:48:04 +0300

    Added Math class with five mathematical methods

---
+++ src/Math.java
@@ -0,0 +1,21 @@
+public class Math {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(int a) {
+        return java.lang.Math.sqrt(a);
+    }
+}
