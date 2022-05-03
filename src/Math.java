19836e67b690f83f7c2dbe17bc9844f5c37f080b
Oleksii Sergiienko <sai.79.ua@gmail.com>
2022-05-03 17:47:33 +0300

    Added add method with 3 parameters

--- src/Math.java
+++ src/Math.java
@@ -1,7 +1,12 @@
 public class Math {
     public int add(int a, int b) {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
         return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
+        return a + b + c;
     }
 
     public int sub(int a, int b) {
