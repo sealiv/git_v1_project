4a2d460b3b2d76061b73082354d4f34a5166be5e
Oleksii Sergiienko <sai.79.ua@gmail.com>
2022-05-10 10:03:02 +0300

    Added support large numbers in math operations.

--- src/Math.java
+++ src/Math.java
@@ -1,8 +1,15 @@
+import java.math.BigInteger;
+
 public class Math {
     public int add(int a, int b) {
         System.out.println("This is add method");
         System.out.println("This is add method for 2 params");
         return a + b;
+    }
+
+    public BigInteger add(BigInteger a, BigInteger b) {
+        System.out.println("This is add method for 2 Biginteger params");
+        return a.add(b);
     }
 
     public int add(int a, int b, int c) {
@@ -19,12 +26,24 @@ public class Math {
         return a - b;
     }
 
+    public BigInteger sub(BigInteger a, BigInteger b) {
+        return a.subtract(b);
+    }
+
     public int div(int a, int b) {
         return a / b;
     }
 
+    public BigInteger div(BigInteger a, BigInteger b) {
+        return a.divide(b);
+    }
+
     public int mult(int a, int b) {
         return a * b;
+    }
+
+    public BigInteger mult(BigInteger a, BigInteger b) {
+        return a.multiply(b);
     }
 
     public double sqrt(int a) {
